7b7f7eb72d9d69332e99528a605198f146c27490
armandossrecife <armando.sousa@gmail.com>
2021-09-25 16:55:54 -0300

    Alterado para suportar banco de dados e modelo.

--- market.py
+++ market.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f'Item {self.name}'
 
 @app.route('/')
 @app.route('/home')
@@ -13,7 +27,5 @@ def about(username):
 
 @app.route('/market')
 def market_page():
-    items = [
-        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500}
-    ]
+    items = Item.query.all()
     return render_template('market.html', items=items)
