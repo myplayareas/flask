27a21c502ef151ed6b0dc68f0120091609edb3a0
armandossrecife <armando.sousa@gmail.com>
2021-09-25 14:22:49 -0300

    Estrutura inicial, estilos, templates e envio de dados.

---
+++ market.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/about/<username>')
+def about(username):
+    return f'Welcome to Flask {username}'
+
+@app.route('/market')
+def market_page():
+    items = [
+        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500}
+    ]
+    return render_template('market.html', items=items)
